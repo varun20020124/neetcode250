d90ff077d6b69c9a0937421e33af016c1b428284
Varun Jhaveri <80193655+varun20020124@users.noreply.github.com>
2026-09-16 22:29:20 -0400

    Add: redundant-connection - submission-4

---
+++ Data Structures & Algorithms/redundant-connection/submission-4.py
@@ -0,0 +1,28 @@
+from collections import deque
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        graph = defaultdict(list)
+        for u,v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+        def bfs(u,v):
+            graph[u].remove(v)
+            graph[v].remove(u)
+            q = deque()
+            q.append(u)
+            visited = set()
+            visited.add(u)
+            while q:
+                node = q.popleft()
+                for adj in graph[node]:
+                    if adj not in visited:
+                        q.append(adj)
+                        visited.add(adj)
+            graph[u].append(v)
+            graph[v].append(u)
+            return len(visited) == len(edges)
+        result = []
+        for (u,v) in edges:
+            if bfs(u,v):
+                result.append([u,v])
+        return result[-1]
